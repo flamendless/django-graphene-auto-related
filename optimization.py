a128d2f296612ba0be7079da27a0b72e102989cc
flamendless <flamendless8@gmail.com>
2023-05-30 13:20:08 +0800

    Implement nested valid fields

--- optimization.py
+++ optimization.py
@@ -1,12 +1,19 @@
-from typing import Optional, Dict, List, Any, KeysView
+from typing import Optional, Dict, List, Any, KeysView, Tuple
+from pprint import pprint
 
-from django.db import models
+from django.db.models import QuerySet
 from django.db.models.fields.related_descriptors import ReverseManyToOneDescriptor
-from versatileimagefield.fields import VersatileImageFileDescriptor
-from .string import to_snake_case
+
+from saleor.core.utils.string import to_snake_case
 
 
-def get_valid_fields(model: Any, tree: Dict, keys: KeysView, root: Any=None) -> List[str]:
+def get_valid_fields(
+    model: Any,
+    tree: Dict,
+    keys: KeysView,
+    root: Any=None
+) -> Tuple[List[str], List[str]]:
+    valid_keys: List[str] = []
     valid_fields: List[str] = []
 
     for field in keys:
@@ -14,12 +21,7 @@ def get_valid_fields(model: Any, tree: Dict, keys: KeysView, root: Any=None) -> 
             continue
 
         if hasattr(model, field):
-            model_field = getattr(model, field)
-
-            if isinstance(
-                model_field,
-                (ReverseManyToOneDescriptor, VersatileImageFileDescriptor)
-            ):
+            if isinstance(getattr(model, field), ReverseManyToOneDescriptor):
                 continue
 
             valid_fields.append(field)
@@ -27,13 +29,18 @@ def get_valid_fields(model: Any, tree: Dict, keys: KeysView, root: Any=None) -> 
 
         if (root is not None) and (hasattr(root, field)):
             next_keys: KeysView = tree[field].keys()
-            others: List[str] = get_valid_fields(model, tree, next_keys, root)
-            valid_fields.extend(others)
+            others, other_keys = get_valid_fields(model, tree, next_keys, root)
+            if others:
+                valid_keys.append(field)
+                valid_fields.extend(others)
 
-    return valid_fields
+            if other_keys:
+                valid_keys.extend(other_keys)
+
+    return valid_fields, valid_keys
 
 
-def auto_related(qs: models.QuerySet, info, root: Any=None) -> models.QuerySet:
+def auto_related(qs: QuerySet, info, root: Any=None) -> QuerySet:
     tree: Optional[Dict] = getattr(info.context, "tree", None)
     if not tree:
         return qs
@@ -46,8 +53,22 @@ def auto_related(qs: models.QuerySet, info, root: Any=None) -> models.QuerySet:
         else tree.keys()
     )
 
-    valid_fields: List[str] = get_valid_fields(model, tree, keys, root)
+    valid_fields, valid_keys = get_valid_fields(model, tree, keys, root)
     if not valid_fields:
         return qs
+
+    if valid_keys:
+        for vf, vk in zip(valid_fields, valid_keys):
+            subtree: Dict = tree[vk][vf]
+            subkeys: List[str] = [
+                subkey
+                for subkey in subtree.keys()
+                if subkey != "fields"
+            ]
+            additional_fields: List[str] = [
+                f"{vf}__{subkey}"
+                for subkey in subkeys
+            ]
+            valid_fields.extend(additional_fields)
 
     return qs.select_related(*valid_fields)
